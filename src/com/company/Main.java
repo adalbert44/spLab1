59654e46278c568741e2b41456150d1a848cc448
Adalbert Makarovych <amakarovych-ua@memsql.com>
2020-09-06 00:55:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    private static final int maxWordLength = 30; // words greater then this value will be truncated
+    private static int max = 0; // maximum count of different letters in the word
+    private static ArrayList<String> maxLettersCountWords = new ArrayList<>(); // array with all words of the length equal to max
+
+    private static void processNewWord(String word) {
+        // count the number of different letters
+        HashSet<Character> letters = new HashSet<>();
+        for (Character c:word.toLowerCase().toCharArray()) {
+            letters.add(c);
+        }
+
+        // update the answer
+        if (letters.size() > max) {
+            max = letters.size();
+            maxLettersCountWords.clear();
+        }
+        if(letters.size() == max) {
+            maxLettersCountWords.add(word);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            File myObj = new File("input.txt");
+            Scanner myReader = new Scanner(myObj);
+
+            StringBuilder lastWord = new StringBuilder();
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                for (Character c:line.toCharArray()) {
+                    if (Character.isLetter(c)) {
+                            lastWord.append(c);
+                    } else {
+                        if (lastWord.length() != 0) {
+                            processNewWord(lastWord.toString());
+                        }
+                        lastWord = new StringBuilder();
+                    }
+                }
+                if (lastWord.length() != 0) {
+                    processNewWord(lastWord.toString());
+                }
+                lastWord = new StringBuilder();
+            }
+
+            // output the results
+            System.out.printf("Maximum count of different letters in the word: %d\n", max);
+            for (String word:maxLettersCountWords) {
+                System.out.println(word);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File doesn't exist");
+            e.printStackTrace();
+        }
+    }
+}
